c6fee3afb2dd8da33478e71ddc4ccf9c9330143a
Pedro Maia <pedrodelmonico@hotmail.com>
2023-11-14 16:54:07 -0300

    finalizando formulario de cadastro de funcionario

---
+++ src/scripts/funcionarios.js
@@ -0,0 +1,54 @@
+const Trabalhadores = []
+
+class Funcionario {
+    constructor(nomeFuncionario, contaBancaria, CPF, email, genero, dataNasc, Cargo, Salario) {
+        this.nome = nomeFuncionario
+        this.contaBancaria = contaBancaria
+        this.cpf = CPF
+        this.email = email
+        this.genero = genero
+        this.dataNasc = dataNasc
+        this.cargo = Cargo
+        this.salario = Salario
+    }
+}
+
+class Professor extends Funcionario {
+    constructor(nomeFuncionario, contaBancaria, CPF, email, genero, dataNasc) {
+        super(nomeFuncionario, contaBancaria, CPF, email, genero, dataNasc, 'Professor', 2164.61)
+    }
+}
+
+class Secrataria extends Funcionario {
+    constructor(nomeFuncionario, contaBancaria, CPF, email, genero, dataNasc) {
+        super(nomeFuncionario, contaBancaria, CPF, email, genero, dataNasc, 'Secretária', 1900.00)
+    }
+}
+
+class RH extends Funcionario {
+    constructor(nomeFuncionario, contaBancaria, CPF, email, genero, dataNasc) {
+        super(nomeFuncionario, contaBancaria, CPF, email, genero, dataNasc, 'RH', 3431.00)
+    }
+}
+
+class TI extends Funcionario {
+    constructor(nomeFuncionario, contaBancaria, CPF, email, genero, dataNasc) {
+        super(nomeFuncionario, contaBancaria, CPF, email, genero, dataNasc, 'TI', 4300.00)
+    }
+}
+
+const Professor1 = new Professor('Teadoro Campos', 13072533, 26895242015, 'teodoro@hotmail.com', 'masculino', '1985-10-11')
+const Professor2 = new Professor('Fernando Campos', 2991004, 83126313990, 'fernando@hotmail.com', 'masculino', '1990-03-13')
+const Professor3 = new Professor('Hugo Rodrigues', 270821142, 78089575269, 'hugo@hotmail.com', 'masculino', '1988-01-12')
+const Professor4 = new Professor('Júlia Henriques', 11392148, 50666911703, 'julia@hotmail.com', 'feminino', '1977-03-10')
+
+const Secrataria1 = new Secrataria('Marli Benzerra', 10277684, 67992512464, 'marli@hotmail.com', 'feminino', '1995-07-15')
+
+const Rh1 = new RH('Bárbara Catilho', 2634058, 94973111171, 'barbara@hotmail.com', 'feminino', '2003-08-06')
+const Rh2 = new RH('Marcelo Carvalho', 16525758, 64132885828, 'marcelo@hotmail.com', 'masculino', '1979-02-16')
+
+const TI1 = new TI('Saulo Borges', 152906431, 46401993928, 'saulo@hotmail.com', 'masculino', '1984-06-04')
+
+Trabalhadores.push(Professor1, Professor2, Professor3, Professor4, Secrataria1, Rh1, Rh2, TI1)
+
+export { Trabalhadores, Professor, Secrataria, RH, TI }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
+import { Trabalhadores, Professor, Secrataria, RH, TI } from "./funcionarios"
+
 document.addEventListener("DOMContentLoaded", () => {
     const buttonEntrar = document.querySelector('form')
     const homePage = document.querySelector('.home-page')
@@ -6,5 +8,62 @@ document.addEventListener("DOMContentLoaded", () => {
         e.preventDefault()
         homePage.style.display = 'none';
     })
+
+    TrocaConteudo()
+    Funcionarios()
 })
 
+function TrocaConteudo() {
+    const liItems = document.querySelectorAll('.main-page__side-bar__container__item')
+    const contents = document.querySelectorAll('.main-page__content > div')
+
+    liItems.forEach((item, indice) => {
+        item.addEventListener('click', () => {
+            //removendo o modificador --active
+            liItems.forEach(otherItem => {
+                otherItem.classList.remove('main-page__side-bar__container__item--active');
+            });
+            //adicionando o modificador no item q foi clicado
+            item.classList.add('main-page__side-bar__container__item--active');
+
+            //oculta todos os conteudos
+            contents.forEach(item => item.style.display = 'none')
+
+            //mostra somente o conteudo referente ao indice
+            contents[indice].style.display = 'flex';
+        });
+    });
+}
+
+function Funcionarios() {
+    const inputs = document.querySelectorAll('.main-page__content__funcionarios__cadastrar__form__input > input, .main-page__content__funcionarios__cadastrar__form__input > select')
+    const form = document.querySelector('.main-page__content__funcionarios__cadastrar__form')
+    const opcao = document.getElementById('profissoes')
+
+    let novoFuncionario = ''
+
+    form.addEventListener('submit', () => {
+
+        if (opcao.value === 'professor') {
+            novoFuncionario = new Professor(inputs[0].value, inputs[1].value, inputs[2].value, inputs[3].value, inputs[4].value, inputs[5].value)
+            Trabalhadores.push(novoFuncionario)
+        }
+
+        if (opcao.value === 'Rh') {
+            novoFuncionario = new RH(inputs[0].value, inputs[1].value, inputs[2].value, inputs[3].value, inputs[4].value, inputs[5].value)
+            Trabalhadores.push(novoFuncionario)
+        }
+
+        if (opcao.value === 'Ti') {
+            novoFuncionario = new TI(inputs[0].value, inputs[1].value, inputs[2].value, inputs[3].value, inputs[4].value, inputs[5].value)
+            Trabalhadores.push(novoFuncionario)
+        }
+
+        if (opcao.value === 'secretaria') {
+            novoFuncionario = new Secrataria(inputs[0].value, inputs[1].value, inputs[2].value, inputs[3].value, inputs[4].value, inputs[5].value)
+            Trabalhadores.push(novoFuncionario)
+        }
+
+        console.log(Trabalhadores)
+    })
+}
